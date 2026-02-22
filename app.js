a933b7fac38cf5834ac3f27016f75ec1065a21b3
Sveta <artist.sv.chepurna@gmail.com>
2026-02-22 20:49:41 +0100

    Debugging Exercises

--- app.js
+++ app.js
@@ -1,91 +1,85 @@
 // 1 Exercise 'Array Length':
 // Create a function that counts the number of countries in the array and returns the count.
-// Define the variable inside the function.
-function countCountries() {
-   let exerciseArray = ["Spain", "France", "Italy", "Germany", "Portugal"];
+function countCountries(exerciseArray) {
+   
    let countNum = 0; 
    for (let i = 0; exerciseArray[i] !== undefined; i++) {
       countNum++;
    }
    return countNum;
 }
-// Define the variable outside the function. Call the function, initialize reterned value to our variable
-//  and log the result to the console.
-let resultNum = 0;
-console.log(countCountries(resultNum)); 
-// Output: 5
 
+console.log(countCountries(["Spain", "France", "Italy", "Germany", "Portugal"]));
+// Output: 5
 
 // ******************
 // 2 Exercise 'Print Elements with Index':
 // Create a function that prints the array items with their corresponding index number.
-function nameP() {
-    let newArray = ["John", "Jane", "Doe", "Mary", "Smith"];
+function nameP(newArray) {
+    
     let index = 0;
     while (newArray[index] !== undefined) {
         console.log(index + "." + newArray[index]);
         index++;
     }
 }
-nameP(); 
+let newArray = ["John", "Jane", "Doe", "Mary", "Smith"];
+nameP(newArray); 
 // Output: 0.John 1.Jane 2.Doe 3.Mary 4.Smith      
 
 
 // ******************
 // 3 Exercise 'Sum of Numbers':
 // Create a function that sums all the numbers in an array and returns the result.
-let numArray = [1, 2, 3, 4, 5];
-let count = 0;
 
-function countNum() {
+function sumArray(array) {
+    let sum = 0;
     let i = 0;
     do {
-        count += numArray[i];
+        sum += array[i];
         i ++;   
 
-    } while (i < numArray.length);
+    } while (i < array.length);
 
-    return count;
+    return sum;
 }
-console.log(countNum()); 
+console.log(sumArray([1, 2, 3, 4, 5])); 
 // Output: 15
+
+
 
 
 // 4 Exercise 'Find the Largest Number':
 // Create a function that finds the largest number in an array and returns it.
+
+function compareMount(mountArray) {
+    let largest = mountArray[0][1];
+    let num = 0;
+    for (i = 1; i < mountArray.length; i++) {
+       num = mountArray[i][1];  
+          if (num > largest) {
+                largest = num;
+          }
+    }
+    // Return the final result after loop finished
+    return largest;
+}
 let mountArray = [
     ["Makalu", 8485],
-    ["Mount Everest", 8849],
     ["K2", 8611],
     ["Kangchenjunga", 8586],
     ["Lhotse", 8516],
     ["Cho Oyu", 8188],
+    ["Mount Everest", 8849],
     ["Dhaulagiri", 8167],
     ["Manaslu", 8163],
     ["Nanga Parbat", 8126],
 ];
 
-let largestMount = []; 
-let mountCount = mountArray.length;
-
-function compareMount() {
-    let index = 0;
-    let num1 = 0;
-    let num2 = 0;
-    while (index < mountCount) {
-       num1 = mountArray[index][1]; 
-       index++;
-       num2 = mountArray[index][1];
-       
-          if (num1 < num2) {
-             return largestMount = num2;
-          } else {
-              return largestMount = num1;
-         }
-        }
-}
-console.log("The largest mount height is " + compareMount() + " meters."); 
+console.log("The largest mount height is " + compareMount(mountArray) + " meters."); 
 // Output: The largest mount height is 8849 meters.
+
+
 
 
 // 5 Exercise 'Reverse an Array':
@@ -104,8 +98,7 @@ console.log(reverseArray());
 
 // 6 Exercise 'Count a Value':
 // Create a function that counts the number of times a value appears in an array and returns the count.
-function x(valueX) {
-    let arrayX = ["blue", "red", "green", "blue", "yellow", "blue", "red"];
+function x(valueX, arrayX) {
     let item = "";
     let countX = 0;
     for (let j = 0; j < arrayX.length; j++ ) {
@@ -119,21 +112,23 @@ function x(valueX) {
     return countX;
 }
 let valueX = "blue";
-console.log(x(valueX)); 
+let arrayX = ["blue", "red", "green", "blue", "yellow", "blue", "red"];
+console.log(x(valueX, arrayX)); 
 // Output: 3
 
 
 // 7 Exercise 'String Loop':
 // Create a function that prints each character of a string on a new line.
-function strPrint() {
-    let str = "This string was printed using a loop.";
+function strPrint(str) {
+
     let index = 0;
     while (index < str.length) {
         console.log(str[index]);
         index++;
     }
 }
-strPrint();
+let str = "This string was printed using a loop.";
+strPrint(str);
 // Output: T h i s   s t r i n g   w a s   p r i n t e d   u s i n g   a   l o o p .
 
 
@@ -166,4 +161,3 @@ function pattPr(y) {
 let y = 3;
 pattPr(y);
 // Output: * ** ***
-
